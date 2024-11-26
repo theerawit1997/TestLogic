1e2797c06c6bd4cce7e6003a9b1b1892200f235a
theerawit1997 <theerawit.towork@gmail.com>
2024-11-26 11:47:45 +0700

    Create getMaxValueInArray.js

---
+++ getMaxValueInArray.js
@@ -0,0 +1,12 @@
+function getMaxValueInArray(arr) {
+    let maxvalue = 0;
+    for (let index = 0; index < arr.length; index++) {
+      if (arr[index] >= maxvalue) {
+        maxvalue = arr[index];
+      }
+    }
+    return maxvalue;
+  } 
+  
+  const thisarry = [1,2,3,4,5,6,7,8,10,1,2,3,5,6,8,7,12,35,0,8];
+  console.log(getMaxValueInArray(thisarry));
