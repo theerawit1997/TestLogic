1698bd2b8d99d1529841ebc9ffae9eda843b4950
theerawit1997 <theerawit.towork@gmail.com>
2025-01-08 13:34:57 +0700

    Update getMinValueInArray.js

--- getMinValueInArray.js
+++ getMinValueInArray.js
@@ -1,11 +1,11 @@
 function getMinValueInArray(arr) {
-    let maxvalue = 0;
+    let minvalue = 0;
     for (let index = 0; index < arr.length; index++) {
-      if (arr[index] < maxvalue) {
-        maxvalue = arr[index];
+      if (arr[index] < minvalue) {
+        minvalue = arr[index];
       }
     }
-    return maxvalue;
+    return minvalue;
   } 
   
   const thisarry = [1,2,3,4,5,6,7,8,-10,1,2,3,5,6,8,7,12,35,0,8];
